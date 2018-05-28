4a87e5742ede0b4e96e420f522784bdaed9e5cb9
Anne Gill <gill01730@gmail.com>
2018-05-28 16:07:57 +0000

    CHANGED DATABASE

--- saint_peter_gis/settings/base.py
+++ saint_peter_gis/settings/base.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +25,7 @@ SECRET_KEY = '&-iy8v6(qjx=zb510vng6zh7-&d05s9aq0b#0b@-4kmhfk(vwe'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['saint-peter-gis-agill1.c9users.io' ]
+ALLOWED_HOSTS = ['saint-peter-gis-agill1.c9users.io','saint-peter-gis.herokuapp.com' ]
 
 
 # Application definition
@@ -79,10 +79,7 @@ WSGI_APPLICATION = 'saint_peter_gis.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3"))
 }
 
 
